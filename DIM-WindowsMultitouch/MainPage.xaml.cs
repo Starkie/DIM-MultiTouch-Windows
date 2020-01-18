131c7364937eb71e3df26bbdb276052a46e3f4c1
Adriano Vega Llobell <adrianovega@hotmail.com>
2020-01-18 21:27:50 +0100

    Corregido el namespace y cambiado a Dim.MultiTouch

--- DIM-WindowsMultitouch/MainPage.xaml.cs
+++ DIM-WindowsMultitouch/MainPage.xaml.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace DIM_WindowsMultitouch
+﻿namespace Dim.MultiTouch
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary> An empty page that can be used on its own or navigated to within a Frame. </summary>
     public sealed partial class MainPage : Page
     {
+        /// <summary> Initializes a new instance of the <see cref="MainPage"/> class. </summary>
         public MainPage()
         {
             this.InitializeComponent();
