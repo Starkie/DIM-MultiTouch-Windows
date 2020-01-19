0a5bb8d66fecb3eea6d63949d519831f6b70b5b0
Adriano Vega Llobell <adrianovega@hotmail.com>
2020-01-19 11:01:06 +0100

    Renamed the folder to src

--- src/Excercise1/App.xaml.cs
+++ src/Excercise1/App.xaml.cs
@@ -42,7 +42,7 @@ namespace Dim.MultiTouch
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: Load state from previously suspended application
+                    // TODO: Load state from previously suspended application
                 }
 
                 // Place the frame in the current Window
@@ -82,7 +82,7 @@ namespace Dim.MultiTouch
         {
             var deferral = e.SuspendingOperation.GetDeferral();
 
-            //TODO: Save application state and stop any background activity
+            // TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
